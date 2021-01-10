761993980fb3697efe7f4a4694f54d6920691bfe
MajkiSVK <44mikulas.tomas@gmail.com>
2021-01-10 18:43:40 +0100

    created master blade and empty style.css

---
+++ resources/views/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
+
+        <!-- My CSS -->
+        <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+
+        <title>Turistika na Slovensku</title>
+    </head>
+    <body class="{{ Request::segment(1) ?: 'main' }}">
+        <main>
+            <!-- Class yielding for customization specific pages -->
+            <div class="container @yield('class')">
+                <!-- Yielding the main content of the page -->
+                @yield('container')
+            </div>
+
+        </main>
+    </body>
+</html>
