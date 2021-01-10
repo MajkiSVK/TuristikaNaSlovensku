2a6a44b2206ea22c89379147cd3c8fe502963aaf
MajkiSVK <44mikulas.tomas@gmail.com>
2021-01-10 22:14:02 +0100

    implemented flash messages

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,12 +10,17 @@
         <!-- My CSS -->
         <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
 
+        <!-- Scripts -->
+        <script src="{{ URL::asset('js/app.js') }}" defer></script>
+
         <title>Turistika na Slovensku</title>
     </head>
     <body class="{{ Request::segment(1) ?: 'main' }}">
         <main>
             <!-- Class yielding for customization specific pages -->
             <div class="container @yield('class')">
+
+                @include('partials.flash_messages')
                 <!-- Yielding the main content of the page -->
                 @yield('container')
             </div>
---
+++ resources/views/partials/flash_messages.blade.php
@@ -0,0 +1,42 @@
+@if ($message = Session('success'))
+    <div class="alert alert-success alert-block mt-2">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session('error'))
+    <div class="alert alert-danger alert-block mt-2">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session('warning'))
+    <div class="alert alert-warning alert-block mt-2">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session('info'))
+    <div class="alert alert-info alert-block mt-2">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session('top3'))
+    <div class="alert alert-info alert-block mt-2">
+        <button type="button" class="close" data-dismiss="alert" onclick="{{session()->forget('top3')}}">×</button>
+        <strong>{{ $message }}</strong>
+        <a href="{{ URL::route('monthly_best_create')}}" class="btn btn-dark">Prejsť na vytváranie</a>
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger mt-2">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        Please check the form below for errors
+    </div>
+@endif
