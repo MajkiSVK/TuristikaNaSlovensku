c89070c2ad60776a27f307700ad016c5c0f93fb2
MajkiSVK <44mikulas.tomas@gmail.com>
2021-01-16 18:19:16 +0100

    added links for title img and name to home page

--- resources/views/pages/contest/gallery.blade.php
+++ resources/views/pages/contest/gallery.blade.php
@@ -9,10 +9,10 @@ contest_gallery
 
 <div class="row justify-content-between ">
     <div class="d-none d-md-block col-md-4 text-break mt-3"></div>
-   <div class=" col-12 col-md-4 text-center mt-2"> <img src="{{asset('storage/tns.png')}}" class="w-100"> </div>
+   <div class=" col-12 col-md-4 text-center mt-2"> <a href="{{URL::to(route('home'))}}"><img src="{{asset('storage/tns.png')}}" class="w-100"> </a></div>
     {{--If the user is logged in, show the Logout button, else show the Login button--}}
     @if(session()->get('name'))
-        <div class="col-12 col-md-4 text-right text-break mt-3"><b>{{session()->get('name')}}</b>
+        <div class="col-12 col-md-4 text-right text-break mt-3"><b><a href="{{URL::to(route('home'))}}">{{session()->get('name')}}</a></b>
         <a href="{{URL::to(route('FbLogout'))}}" class="btn btn-primary text-break">Odhlásiť sa</a></div>
     @else
         <div class="col-12 col-md-4 text-right text-break mt-3">
