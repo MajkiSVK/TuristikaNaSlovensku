8db978b411d70ea8af34dbecaf975450d67926aa
MajkiSVK <44mikulas.tomas@gmail.com>
2021-01-17 16:10:43 +0100

    created photo save function and added comments in photo repository

--- app/Repository/PhotoRepository.php
+++ app/Repository/PhotoRepository.php
@@ -7,18 +7,42 @@ use App\Photo;
 
 class PhotoRepository
 {
+    /*
+     * Get the photo with Contest_id and photo ID
+     */
     public function GetByContestIdFirstOrFail($contest,$request)
     {
         return Photo::where('contest_id',$contest->id)->where('id', $request->photo_id)->firstOrFail();
     }
 
+    /*
+     * Get next photo ID
+     */
     public function GetNextId($contest,$photo)
     {
         return Photo::where('contest_id',$contest->id)->where('id','>',$photo->id)->min('id');
     }
 
+    /*
+     * Get previous photo ID
+     */
     public function GetPrevId($contest,$photo)
     {
         return Photo::where('contest_id',$contest->id)->where('id','<',$photo->id)->max('id');
+    }
+
+    /*
+     * Save the photo information
+     */
+    public function Save($description,$user_id,$original_path,$resized_path,$thumb_path)
+    {
+        $photo=new Photo();
+        $photo->description = $description;
+        $photo->user_id=$user_id;
+        $photo->original_path=$original_path;
+        $photo->resized_path=$resized_path;
+        $photo->thumb_path=$thumb_path;
+
+        return $photo->save();
     }
 }
