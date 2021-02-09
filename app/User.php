af10b58636337549bdc3ce2b16a8f2f5f6c41d0c
MajkiSVK <44mikulas.tomas@gmail.com>
2021-02-09 17:34:09 +0100

    relationship bug fixed

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function settings()
     {
-        $this->hasMany('App\UserSetting');
+        return $this->hasMany('App\UserSetting');
     }
 
 }
