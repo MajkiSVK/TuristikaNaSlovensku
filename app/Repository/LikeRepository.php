8e0a9e4b63417d24b86d99d69a37e81dbf56703b
MajkiSVK <44mikulas.tomas@gmail.com>
2021-06-16 20:39:04 +0200

    code formatting in LikeRepository

--- app/Repository/LikeRepository.php
+++ app/Repository/LikeRepository.php
@@ -1,25 +1,29 @@
 <?php
 
-
 namespace App\Repository;
-
 
 use App\Like;
 
 class LikeRepository
 {
-    /*
+    /**
      * Check, if a user already voted for the photo
+     * @param int $facebook_id
+     * @param string $url
+     * @return mixed
      */
-    public function check_like($facebook_id,$url)
+    public function check_like(int $facebook_id,string $url)
     {
         return Like::where('facebook_id',$facebook_id)->where('URL', $url)->first();
    }
 
-   /*
-    * Add like for photo
-    */
-    public function add_like($facebook_id,$url)
+    /**
+     * Add like for photo
+     * @param int $facebook_id
+     * @param string $url
+     * @return bool
+     */
+    public function add_like(int $facebook_id,string $url): bool
     {
         $new_like=new Like();
         $new_like->facebook_id=$facebook_id;
@@ -47,10 +51,12 @@ class LikeRepository
         return  Like::where('facebook_id',$facebook_id)->where('URL', $url)->first();
     }
 
-    /*
+    /**
      * Like counter for specific URL
+     * @param string $url
+     * @return int
      */
-    public function likeCounter($url)
+    public function likeCounter(string $url): int
     {
         return Like::where('URL', $url)->count();
     }
